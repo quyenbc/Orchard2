7f77b34a8d4ea6ce134c0f261ff63e06ef052710
Sebastien Ros <sebastienros@gmail.com>
2016-11-19 20:16:33 -0800

    Added "container" block helper

--- src/Orchard.Cms.Web/Modules/Orchard.Tokens.Content/ContentTokens.cs
+++ src/Orchard.Cms.Web/Modules/Orchard.Tokens.Content/ContentTokens.cs
@@ -9,17 +9,47 @@ namespace Orchard.Tokens
     {
         public static void RegisterContentTokens(this IHandlebars handlebars)
         {
+            // Renders a slug for the current content item. Do not use to represent the url
+            // as the content item as it might be different than the computed slug.
             handlebars.RegisterHelper("slug", (output, context, arguments) =>
             {
                 IServiceProvider serviceProvider = context.ServiceProvider;
                 var contentManager = serviceProvider.GetRequiredService<IContentManager>();
-
+                
                 ContentItem contentItem = context.Content;
-
+                                
                 string title = contentManager.GetItemMetadata(contentItem).DisplayText;
 
                 var slug = title?.ToLower().Replace(" ", "-");
                 output.Write(slug);
+            });
+
+            // The "container" blog helper redifines the context.Content property to
+            // the container of the current context Content property. If the content doesn't
+            // have a container then the inner template is not rendered.
+            // Example: {{#container}}{{slug}}/{{/container}}{{slug}}, this will render the slug of the 
+            // container then the slug of the content item.
+            handlebars.RegisterHelper("container", (output, options, context, arguments) =>
+            {
+                ContentItem contentItem = context.Content;
+
+                int? containerId = contentItem.Content?.ContainedPart?.ListContentItemId;
+
+                if (containerId.HasValue)
+                {
+                    IServiceProvider serviceProvider = context.ServiceProvider;
+                    var contentManager = serviceProvider.GetRequiredService<IContentManager>();
+
+                    var container = contentManager.GetAsync(containerId.Value).Result;
+
+                    if (container != null)
+                    {
+                        var previousContent = context.Content;
+                        context.Content = container;
+                        options.Template(output, context);
+                        context.Content = previousContent;
+                    }
+                }
             });
         }
     }
